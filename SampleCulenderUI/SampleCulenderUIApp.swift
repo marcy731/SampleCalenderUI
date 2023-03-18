1470fce9440e5b9ba85e2dc8b0d05cf5f4dfc3d4
marcy731 <marcy731@hey.jp>
2023-03-18 09:09:39 +0900

    Initial Commit

---
+++ SampleCulenderUI/SampleCulenderUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCulenderUIApp.swift
+//  SampleCulenderUI
+//
+//  Created by marcy731 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct SampleCulenderUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
